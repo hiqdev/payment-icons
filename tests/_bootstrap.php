4356e102213dc24f65460b9df3762263c16ac48e
Andrii Vasyliev <sol@hiqdev.com>
2015-12-15 16:19:20 +0000

    fixed tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -9,10 +9,10 @@
  * @copyright Copyright (c) 2015, HiQDev (http://hiqdev.com/)
  */
 
-error_reporting(-1);
+error_reporting(E_ALL & ~E_NOTICE);
 
 require_once __DIR__ . '/../vendor/autoload.php';
-#require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 Yii::setAlias('@vendor', dirname(__DIR__) . '/vendor');
 Yii::setAlias('@hiqdev/yii2/paymenticons', dirname(__DIR__));
 Yii::setAlias('@hiqdev/yii2/paymenticons/tests', __DIR__);
